68d14d9a586f3c2615a9dbbf35b5284d3b61c6a9
sara-khatoon <sarakhatoon1003@gmail.com>
2025-03-10 20:47:53 +0530

    dynamicqueue

---
+++ DynamicQueue.c
@@ -0,0 +1,65 @@
+//  implementation of Queue using array using dynamic variables
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n=5,*Queue;
+    int rear = -1, front = -1;
+    int item, *dequeue, i,a;
+    Queue=(int*)malloc(n* sizeof(int));
+    do
+    {
+        printf("Enter your choice:\n");
+        scanf("%d", &a);
+        switch (a)
+        {
+            case 1:
+                if (rear == n - 1)
+                    printf("Queue is full\n");
+                else
+                {
+                    printf("Item to be enqueued: ");
+                    scanf("%d", &item);
+                    if (front == -1)
+                        front = 0;
+                    rear++;
+                    Queue[rear] = item;
+                }
+                break;
+            case 2:
+                if (front == -1 || front > rear)
+                    printf("Queue is empty\n");
+                else
+                {
+                    *dequeue = Queue[front];
+                    printf("Dequeued item: %d\n", *dequeue);
+                    front++;
+                    if (front > rear)
+                    {
+                        front = rear = -1;
+                    }
+                }
+                break;
+            case 3:
+            if (front == -1)
+            printf("Queue is empty\n");
+        else
+        {
+            printf("The values in the queue are:\n");
+            for (i = front; i <= rear; i++)
+            {
+                printf("%d ", Queue[i]);
+            }
+            printf("\n");
+        }
+        break;
+    case 4:
+        printf("Exit\n");
+        return 1;
+    default:
+        printf("Invalid choice\n");
+    }
+} while (1);
+
+return 0;
+}
